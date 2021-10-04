09af699a7e525d4ea74fa3b56143bb260c1f12e1
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-10-04 17:34:53 -0500

    more death_stika work

--- death_sitka.py
+++ death_sitka.py
@@ -6,7 +6,7 @@ open_file = open("sitka_weather_2018_simple.csv", "r")
 open_file2 = open("death_valley_2018_simple.csv", 'r')
 
 csv_file = csv.reader(open_file, delimiter = ',')
-csv_file2 =csv.reader(open_file, delimiter = ',')
+csv_file2 =csv.reader(open_file2, delimiter = ',')
 
 header_row = next(csv_file)
 header_row2 = next(csv_file2)
@@ -15,6 +15,10 @@ header_row2 = next(csv_file2)
 dates = []
 highs = []
 lows = []
+
+dates2 = []
+highs2 = []
+lows2 = []
 
 for row in csv_file:
     try:
@@ -44,17 +48,22 @@ for row in csv_file2:
 print(high)
 print(dates)
 print(low)
+print(highs2)
+print(dates2)
+print(lows2)
 
 import matplotlib.pyplot as plt
 
+fig = plt.figure()
+
 plt.subplot(2,1,1)
-plt.plot(dates, low, c='blue', alpha = 0.5)
-plt.plot(dates, high, c='red', alpha = 0.5)
+plt.plot(dates, lows, c='blue', alpha = 0.5)
+plt.plot(dates, highs, c='red', alpha = 0.5)
 plt.title('Sitka')
 
 plt.subplot(2,1,2)
-plt.plot(dates, low, c='blue', alpha = 0.5)
-plt.plot(dates, high, c='red', alpha = 0.5)
+plt.plot(dates2, lows2, c='blue', alpha = 0.5)
+plt.plot(dates2, highs2, c='red', alpha = 0.5)
 plt.title('Death Valley')
 
 plt.suptitle('Temperatures of Sitka and Death Valley')
